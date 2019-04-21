5cf200f58da59a44fafbe99c5a909e04962e5353
lostsquirrel <songli1988@gmail.com>
2019-04-21 21:40:15 +0800

    expression in jsx

--- hello-world/src/index.js
+++ hello-world/src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const element = <h1>Hello, world!</h1>;
+const name = 'React';
+const hello = <h1>Hello, {name}</h1>;
 
 ReactDOM.render(
-    element,
+    hello,
     document.getElementById('root')
 );
