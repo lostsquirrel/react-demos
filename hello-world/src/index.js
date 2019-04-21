8e701b3e54db5331d45eff3552841a9a68285532
lostsquirrel <songli1988@gmail.com>
2019-04-21 22:06:59 +0800

    state and lifecycle

--- hello-world/src/index.js
+++ hello-world/src/index.js
@@ -2,16 +2,44 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-function Welcome(props) {
-    return <h1>Hello, {props.name}</h1>;
-}
+class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {date: new Date()};
+    }
 
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+        );
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    tick() {
+        this.setState({
+            date: new Date()
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Hello, world!</h1>
+                <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+            </div>
+        );
+    }
+}
 function App() {
     return (
         <div>
-            <Welcome name="Sara" />
-            <Welcome name="Cahal" />
-            <Welcome name="Edite" />
+            <Clock />
+            <Clock />
+            <Clock />
         </div>
     );
 }
