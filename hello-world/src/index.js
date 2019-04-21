59aa6ac3a7b718385e4eaf857527250eb7cac9f6
lostsquirrel <songli1988@gmail.com>
2019-04-21 21:52:19 +0800

    update the rendered element

--- hello-world/src/index.js
+++ hello-world/src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-const element = (
-    <div>
-        <h1>Hello!</h1>
-        <h2>Good to see you here.</h2>
-    </div>
-);
+function tick() {
+    const element = (
+        <div>
+            <h1>Hello, world!</h1>
+            <h2>It is {new Date().toLocaleTimeString()}.</h2>
+        </div>
+    );
+    ReactDOM.render(element, document.getElementById('root'));
+}
 
-ReactDOM.render(
-    element,
-    document.getElementById('root')
-);
+setInterval(tick, 1000);
