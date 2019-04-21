67190c435907a7c25876977af9fc35aafd5385c6
lostsquirrel <songli1988@gmail.com>
2019-04-21 21:48:00 +0800

    children in jsx

--- hello-world/src/index.js
+++ hello-world/src/index.js
@@ -1,23 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function formatName(user) {
-    return user.firstName + ' ' + user.lastName;
-}
 
-const user = {
-    firstName: 'Harper',
-    lastName: 'Perez'
-};
-
-function getGreeting(user) {
-    if (user) {
-        return <h1>Hello, {formatName(user)}!</h1>;
-    }
-    return <h1>Hello, Stranger.</h1>;
-}
-
-const element = getGreeting(user);
+const element = (
+    <div>
+        <h1>Hello!</h1>
+        <h2>Good to see you here.</h2>
+    </div>
+);
 
 ReactDOM.render(
     element,
