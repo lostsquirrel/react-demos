ec83c47f6d637b37b2ffeedc93802b83bc9e8ead
lisong <songli1988@gmail.com>
2019-05-03 16:41:48 +0800

    add simple button

--- hello-world/src/index.js
+++ hello-world/src/index.js
@@ -1,49 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-
-class Clock extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {date: new Date()};
-    }
-
-    componentDidMount() {
-        this.timerID = setInterval(
-            () => this.tick(),
-            1000
-        );
-    }
-
-    componentWillUnmount() {
-        clearInterval(this.timerID);
-    }
-
-    tick() {
-        this.setState({
-            date: new Date()
-        });
-    }
-
-    render() {
-        return (
-            <div>
-                <h1>Hello, world!</h1>
-                <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-            </div>
-        );
-    }
+function activateLasers() {
+    console.log('activateLasers....')
 }
-function App() {
+
+function SimpleButton() {
     return (
-        <div>
-            <Clock />
-            <Clock />
-            <Clock />
-        </div>
+        <button onClick={activateLasers}>
+            Activate Lasers
+        </button>
     );
 }
 ReactDOM.render(
-    <App />,
+    <SimpleButton />,
     document.getElementById('root')
 );
