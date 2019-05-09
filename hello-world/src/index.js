8d09a42f6205b33cd0e9336e99cde1ddb4d7eba5
lostsquirrel <songli1988@gmail.com>
2019-05-09 17:09:18 +0800

    Rendering Multiple Components

--- hello-world/src/index.js
+++ hello-world/src/index.js
@@ -1,44 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-function WarningBanner(props) {
-    if (!props.warn) {
-        return null;
-    }
-
-    return (
-        <div className="warning">
-            Warning!
-        </div>
-    );
-}
-
-class Page extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {showWarning: true};
-        this.handleToggleClick = this.handleToggleClick.bind(this);
-    }
-
-    handleToggleClick() {
-        this.setState(state => ({
-            showWarning: !state.showWarning
-        }));
-    }
-
-    render() {
-        return (
-            <div>
-                <WarningBanner warn={this.state.showWarning}/>
-                <button onClick={this.handleToggleClick}>
-                    {this.state.showWarning ? 'Hide' : 'Show'}
-                </button>
-            </div>
-        );
-    }
-}
+const numbers = [1, 2, 3, 4, 5];
+const listItems = numbers.map((number) =>
+    <li>{number}</li>
+);
 
 ReactDOM.render(
-    <Page/>,
+    <ul>{listItems}</ul>,
     document.getElementById('root')
 );
