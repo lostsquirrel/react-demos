09a1ce30181fa51b98454aae440c4b2715fd61a2
lostsquirrel <songli1988@gmail.com>
2019-05-09 17:24:06 +0800

    8.5 Keys Must Only Be Unique Among Siblings

--- hello-world/src/index.js
+++ hello-world/src/index.js
@@ -1,31 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function ListItem(props) {
-    const value = props.value;
-    return (
-        // Wrong! There is no need to specify the key here:
-        <li>
-            {value}
-        </li>
-    );
-}
-
-function NumberList(props) {
-    const numbers = props.numbers;
-    const listItems = numbers.map((number) =>
-        // Wrong! The key should have been specified here:
-        <ListItem key={number.toString()} value={number} />
-    );
-    return (
+function Blog(props) {
+    const sidebar = (
         <ul>
-            {listItems}
+            {props.posts.map((post) =>
+                <li key={post.id}>
+                    {post.title}
+                </li>
+            )}
         </ul>
     );
+    const content = props.posts.map((post) =>
+        <div key={post.id}>
+            <h3>{post.title}</h3>
+            <p>{post.content}</p>
+        </div>
+    );
+    return (
+        <div>
+            {sidebar}
+            <hr />
+            {content}
+        </div>
+    );
 }
 
-const numbers = [1, 2, 3, 4, 5];
+const posts = [
+    {id: 1, title: 'Hello World', content: 'Welcome to learning React!'},
+    {id: 2, title: 'Installation', content: 'You can install React from npm.'}
+];
 ReactDOM.render(
-    <NumberList numbers={numbers} />,
+    <Blog posts={posts} />,
     document.getElementById('root')
 );
